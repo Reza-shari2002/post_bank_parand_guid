52d30ad584bd423e65093d109088fb8ca4d4186d
Reza <shariatireza25@gmail.com>
2026-06-06 23:35:52 +0330

    added 3

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,20 @@
 import Create_bank_account from "../pages/create_bank_account/Create_bank_account";
+import Home_page from "../pages/home_page/home_page";
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
-import Home_page from "../pages/home_page/home_page"
 
+const router = createHashRouter([
+
+  {
+    path: "/create",
+    element: <Create_bank_account />,
+  },
+]);
 
 function App() {
-const router = createBrowserRouter([{path:"/" , element:<Create_bank_account></Create_bank_account>} , {path:"/home" , element:<Home_page></Home_page>}])
-
-
-  return (
-  
-<><Create_bank_account></Create_bank_account></>
-
-  
-  );
+  return <><RouterProvider router={router} /></>;
 }
 
 export default App;
